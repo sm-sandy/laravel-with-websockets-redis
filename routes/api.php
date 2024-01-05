71e93a56be9562864237b6fd137113756ddb5101
sm-sandy <sm.sandy2021@gmail.com>
2024-01-05 11:16:01 +0600

    update api.php file

--- routes/api.php
+++ routes/api.php
@@ -21,12 +21,3 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/users', [AuthController::class, 'index']);
 });
-
-Route::get('/user-event', function () {
-    $user = [
-        "id" => 1,
-        "name" => "sandy",
-    ];
-    event(new UserDataEvent($user));
-    return null;
-});
